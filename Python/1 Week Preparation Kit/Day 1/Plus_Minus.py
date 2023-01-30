4206ee6e5ad3e0193686a13468b73b6d46d7b448
Shri Krishna Pandey <96811526+shrikrishna97@users.noreply.github.com>
2023-01-30 19:54:21 +0530

    Update Plus_Minus.py

--- Python/1 Week Preparation Kit/Day 1/Plus_Minus.py
+++ Python/1 Week Preparation Kit/Day 1/Plus_Minus.py
@@ -6,6 +6,20 @@ import random
 import re
 import sys
 
+'''Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. 
+Print the decimal value of each fraction on a new line with  places after the decimal.
+
+Note: This challenge introduces precision problems. 
+The test cases are scaled to six decimal places, 
+though answers with absolute error of up to  are acceptable.'''
+'''Example
+arr=[1,1,0,-1,-1]
+There are n=5 elements, two positive, two negative and one zero. Their ratios are 2/5, 2/5 and 1/5. 
+Results are printed as:
+
+0.400000
+0.400000
+0.200000 '''
 #
 # Complete the 'plusMinus' function below.
 #
