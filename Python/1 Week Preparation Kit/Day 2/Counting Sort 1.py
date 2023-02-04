0f1ecb40da0b00da9efc89bd7cd545d1b57ce0b8
Shri Krishna Pandey <96811526+shrikrishna97@users.noreply.github.com>
2023-02-05 00:34:17 +0530

    Create Counting Sort 1.py

---
+++ Python/1 Week Preparation Kit/Day 2/Counting Sort 1.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+""" 
+Challenge
+Given a list of integers, count and return the number of times each value appears as an array of integers.
+
+Function Description
+
+Complete the countingSort function in the editor below.
+
+countingSort has the following parameter(s):
+
+arr[n]: an array of integers
+Returns
+
+int[100]: a frequency array
+"""
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    to = [0]*100
+    le =len(arr)
+    for i in range(0,le):
+        tem = arr[i]
+        to[tem] += 1
+          
+    return to  
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
